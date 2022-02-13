ea7dfa609cd7025544e37bb4c0c07b8d871436cb
nitha-os <nitha@inovicstech.com>
2022-02-13 22:31:38 +0530

    commit

--- sampleHeroku/urls.py
+++ sampleHeroku/urls.py
@@ -20,7 +20,5 @@ from django.conf.urls.static import static
 from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('admin/', admin.site.urls)
 ]
